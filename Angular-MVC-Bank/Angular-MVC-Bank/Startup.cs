fb0651a92d0629ab5eb5b952fcec72361975e30e
Alistair Robson <adrobson@hotmail.com>
2015-03-19 17:58:14 +0000

    First

---
+++ Angular-MVC-Bank/Angular-MVC-Bank/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Angular_MVC_Bank.Startup))]
+namespace Angular_MVC_Bank
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
